f0a2fe1d6c875fae1d3b8c44643494c6e4efbaee
杨利超 <529755212@qq.com>
2016-07-27 17:24:49 +0800

    users创建成功

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,15 @@ class CreateUsersTable extends Migration {
 			$table->string('email')->unique();
 			$table->string('password', 60);
 			$table->rememberToken();
+			$table->char('phone',11)->nullable();
+			$table->tinyInteger('role_id')->default('0');
+			$table->char('real_name',10)->nullable();
+			$table->string('headimg')->nullable();
+			$table->char('com',10)->nullable()->comment('三方登陆类型');
+			$table->string('value')->nullable();
+			$table->string('openid')->nullable();
+			$table->char('last_login_ip',16)->nullable();
+			$table->tinyInteger('is_admin')->default(0)->comment('是否是管理员');
 			$table->timestamps();
 		});
 	}
