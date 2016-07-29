c650b6cb0eebfaa27ecda5b2a308bc94fcca053a
杨利超 <529755212@qq.com>
2016-07-29 18:06:38 +0800

    后台大框架

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,11 +11,75 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
+/******************************************************************
+home ,前台的所有路由
+*****************************************************************/
+Route::group(['prefix'=>'donkey/home' , 'namespace'=>'Home'] , function(){
+	
+	Route::get('/', 'WelcomeController@index');
 
-Route::get('home', 'HomeController@index');
+	Route::get('', 'HomeController@index');
 
+});
+
+
+
+/***************************************************************
+登陆和三方登陆的路由写这里
+****************************************************************/
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
-]);
+]);	
+
+
+/****************************************************************
+admin 后台的所有路由
+*****************************************************************/
+Route::group(['prefix'=>'donkey/admin' , 'namespace'=>'Admin' , 'middleware'=>'AppAdmin'], function(){
+	
+	//网站统计
+	Route::get('/' , ['as'=>'admin.home' , 'uses'=>'AdminController@index']);
+	
+	//用户管理
+	Route::get('user' , ['as'=>'admin.user' , 'uses'=>'UserController@index']);
+	
+	//权限管理
+	Route::get('permission' , ['as'=>'admin.auth' , 'uses'=>'PermissionController@index']);
+	
+	Route::get('role' ,['as'=>'admin.role' , 'uses'=>'RoleController@index']);	
+	
+	//分类管理(日记分类)
+	Route::get('category' , ['as'=>'admin.category' , 'uses'=>'CategoryController@index']);
+	
+	//房源管理
+	Route::get('room', ['as'=>'admin.room' , 'uses'=>'RoomController@index']);
+	
+	//视屏管理
+	Route::get('video' , ['as'=>'admin.video' , 'uses'=>'VideoController@index']);
+	
+	//音频管理
+	Route::get('music' , ['as'=>'admin.music' , 'uses'=>'MusicController@index']);
+	
+	//图片管理
+	Route::get('photo' , ['as'=>'admin.photo' , 'uses'=>'PhotoController@index']);
+	
+	//日记管理
+	Route::get('post' , ['as'=>'admin.post' , 'uses'=>'PostController@index']);
+	
+	//评论管理
+	Route::get('comment' , ['as'=>'admin.comment' , 'uses'=>'CommentController@index']);
+	
+	//消息管理
+	Route::get('notification' , ['as'=>'admin.notification' , 'uses'=>'NotifycationController@index']);
+	
+	//个人中心
+	Route::get('personal' , ['as'=>'admin.personal' , 'uses'=>'PersonalController@index']);
+});
+
+
+
+
+
+
+
---
+++ resources/views/admin/master/common_left.blade.php
@@ -0,0 +1,369 @@
+<ul class="nav nav-list">
+	<li class="active">
+		<a href="{{ url('donkey/admin')}}">
+			<i class="menu-icon fa fa-info"></i>
+			<span class="menu-text"> 网站统计 </span>
+		</a>
+
+		<b class="arrow"></b>
+	</li>
+
+	<li class="">
+		<a href="{{ url('donkey/admin/user') }}">
+			<i class="menu-icon fa fa-users"></i>
+			<span class="menu-text"> 用户管理 </span>
+
+			<!--<b class="arrow fa fa-angle-down"></b>-->
+		</a>
+
+		<b class="arrow"></b>
+
+		<!--<ul class="submenu">
+			<li class="">
+				<a href="#" class="dropdown-toggle">
+					<i class="menu-icon fa fa-caret-right"></i>
+
+					用户列表
+					<b class="arrow fa fa-angle-down"></b>
+				</a>
+
+				<b class="arrow"></b>
+
+				<ul class="submenu">
+					<li class="">
+						<a href="top-menu.html">
+							<i class="menu-icon fa fa-caret-right"></i>
+							用户列表
+						</a>
+
+						<b class="arrow"></b>
+					</li>
+				</ul>
+			</li>
+		</ul>-->
+	</li>
+
+	<li class="">
+		<a href="{{ url('donkey/admin/role') }}" class="dropdown-toggle">
+			<i class="menu-icon fa fa-eye"></i>
+			<span class="menu-text"> 权限管理 </span>
+
+			<b class="arrow fa fa-angle-down"></b>
+		</a>
+
+		<b class="arrow"></b>
+
+		<ul class="submenu">
+			<li class="">
+				<a href="{{ url('donkey/admin/role')}}">
+					<i class="menu-icon fa fa-caret-right"></i>
+					角色管理
+				</a>
+
+				<b class="arrow"></b>
+			</li>
+
+			<li class="">
+				<a href="{{ url('donkey/admin/permission')}}">
+					<i class="menu-icon fa fa-caret-right"></i>
+					权限管理
+				</a>
+
+				<b class="arrow"></b>
+			</li>
+		</ul>
+	</li>
+	
+	<li class="">
+		<a href=" {{ url('donkey/admin/category')}}">
+			<i class="menu-icon fa fa-list"></i>
+			<span class="menu-text"> 分类管理 </span>
+
+			<!--<b class="arrow fa fa-angle-down"></b>-->
+		</a>
+	</li>	
+		
+	<li class="">
+		<a href=" {{ url('donkey/admin/room')}}">
+			<i class="menu-icon fa fa-home"></i>
+			<span class="menu-text"> 房源管理 </span>
+		</a>
+	</li>
+		
+	<li class="">
+		<a href="{{ url('donkey/admin/video')}}">
+			<i class="menu-icon fa fa-video-camera"></i>
+			<span class="menu-text"> 视频管理 </span>
+
+		</a>
+	</li>
+	
+	<li class="">
+		<a href="{{ url('donkey/admin/music')}}">
+			<i class="menu-icon fa fa-music"></i>
+			<span class="menu-text"> 音频管理 </span>
+		</a>
+	</li>
+	
+	<li class="">
+		<a href="{{ url('donkey/admin/photo')}}">
+			<i class="menu-icon fa fa-image"></i>
+			<span class="menu-text"> 图片管理 </span>
+		</a>
+	</li>
+	
+	<li class="">
+		<a href="{{ url('donkey/admin/post')}}" class="dropdown-toggle">
+			<i class="menu-icon fa fa-pencil-square-o"></i>
+			<span class="menu-text"> 日记管理 </span>
+		</a>
+	</li>
+	
+	<li class="">
+		<a href="{{ url('donkey/admin/comment')}}" class="dropdown-toggle">
+			<i class="menu-icon fa fa-comment"></i>
+			<span class="menu-text"> 评论管理 </span>
+		</a>
+	</li>
+	
+	<li class="">
+		<a href="{{ url('donkey/admin/notification')}}" class="dropdown-toggle">
+			<i class="menu-icon fa fa-bullhorn"></i>
+			<span class="menu-text"> 消息管理 </span>
+		</a>
+	</li>
+	
+	<li class="">
+		<a href="{{ url('donkey/admin/personal')}}" class="dropdown-toggle">
+			<i class="menu-icon fa fa-user"></i>
+			<span class="menu-text"> 个人中心 </span>
+		</a>
+	</li>
+	<li class="">
+		<a href="#" class="dropdown-toggle">
+			<i class="menu-icon fa fa-pencil-square-o"></i>
+			<span class="menu-text"> Forms </span>
+
+			<b class="arrow fa fa-angle-down"></b>
+		</a>
+
+		<b class="arrow"></b>
+
+		<ul class="submenu">
+			<li class="">
+				<a href="form-elements.html">
+					<i class="menu-icon fa fa-caret-right"></i>
+					Form Elements
+				</a>
+
+				<b class="arrow"></b>
+			</li>
+
+			<li class="">
+				<a href="form-wizard.html">
+					<i class="menu-icon fa fa-caret-right"></i>
+					Wizard &amp; Validation
+				</a>
+
+				<b class="arrow"></b>
+			</li>
+
+			<li class="">
+				<a href="wysiwyg.html">
+					<i class="menu-icon fa fa-caret-right"></i>
+					Wysiwyg &amp; Markdown
+				</a>
+
+				<b class="arrow"></b>
+			</li>
+
+			<li class="">
+				<a href="dropzone.html">
+					<i class="menu-icon fa fa-caret-right"></i>
+					Dropzone File Upload
+				</a>
+
+				<b class="arrow"></b>
+			</li>
+		</ul>
+	</li>
+
+	<li class="">
+		<a href="widgets.html">
+			<i class="menu-icon fa fa-list-alt"></i>
+			<span class="menu-text"> Widgets </span>
+		</a>
+
+		<b class="arrow"></b>
+	</li>
+
+	<li class="">
+		<a href="calendar.html">
+			<i class="menu-icon fa fa-calendar"></i>
+
+			<span class="menu-text">
+				Calendar
+
+				<!-- #section:basics/sidebar.layout.badge -->
+				<span class="badge badge-transparent tooltip-error" title="2 Important Events">
+					<i class="ace-icon fa fa-exclamation-triangle red bigger-130"></i>
+				</span>
+
+				<!-- /section:basics/sidebar.layout.badge -->
+			</span>
+		</a>
+
+		<b class="arrow"></b>
+	</li>
+
+	<li class="">
+		<a href="gallery.html">
+			<i class="menu-icon fa fa-picture-o"></i>
+			<span class="menu-text"> Gallery </span>
+		</a>
+
+		<b class="arrow"></b>
+	</li>
+
+	<li class="">
+		<a href="#" class="dropdown-toggle">
+			<i class="menu-icon fa fa-tag"></i>
+			<span class="menu-text"> More Pages </span>
+
+			<b class="arrow fa fa-angle-down"></b>
+		</a>
+
+		<b class="arrow"></b>
+
+		<ul class="submenu">
+			<li class="">
+				<a href="profile.html">
+					<i class="menu-icon fa fa-caret-right"></i>
+					User Profile
+				</a>
+
+				<b class="arrow"></b>
+			</li>
+
+			<li class="">
+				<a href="inbox.html">
+					<i class="menu-icon fa fa-caret-right"></i>
+					Inbox
+				</a>
+
+				<b class="arrow"></b>
+			</li>
+
+			<li class="">
+				<a href="pricing.html">
+					<i class="menu-icon fa fa-caret-right"></i>
+					Pricing Tables
+				</a>
+
+				<b class="arrow"></b>
+			</li>
+
+			<li class="">
+				<a href="invoice.html">
+					<i class="menu-icon fa fa-caret-right"></i>
+					Invoice
+				</a>
+
+				<b class="arrow"></b>
+			</li>
+
+			<li class="">
+				<a href="timeline.html">
+					<i class="menu-icon fa fa-caret-right"></i>
+					Timeline
+				</a>
+
+				<b class="arrow"></b>
+			</li>
+
+			<li class="">
+				<a href="email.html">
+					<i class="menu-icon fa fa-caret-right"></i>
+					Email Templates
+				</a>
+
+				<b class="arrow"></b>
+			</li>
+
+			<li class="">
+				<a href="login.html">
+					<i class="menu-icon fa fa-caret-right"></i>
+					Login &amp; Register
+				</a>
+
+				<b class="arrow"></b>
+			</li>
+		</ul>
+	</li>
+
+	<li class="">
+		<a href="#" class="dropdown-toggle">
+			<i class="menu-icon fa fa-file-o"></i>
+
+			<span class="menu-text">
+				Other Pages
+
+				<!-- #section:basics/sidebar.layout.badge -->
+				<span class="badge badge-primary">5</span>
+
+				<!-- /section:basics/sidebar.layout.badge -->
+			</span>
+
+			<b class="arrow fa fa-angle-down"></b>
+		</a>
+
+		<b class="arrow"></b>
+
+		<ul class="submenu">
+			<li class="">
+				<a href="faq.html">
+					<i class="menu-icon fa fa-caret-right"></i>
+					FAQ
+				</a>
+
+				<b class="arrow"></b>
+			</li>
+
+			<li class="">
+				<a href="error-404.html">
+					<i class="menu-icon fa fa-caret-right"></i>
+					Error 404
+				</a>
+
+				<b class="arrow"></b>
+			</li>
+
+			<li class="">
+				<a href="error-500.html">
+					<i class="menu-icon fa fa-caret-right"></i>
+					Error 500
+				</a>
+
+				<b class="arrow"></b>
+			</li>
+
+			<li class="">
+				<a href="grid.html">
+					<i class="menu-icon fa fa-caret-right"></i>
+					Grid
+				</a>
+
+				<b class="arrow"></b>
+			</li>
+
+			<li class="">
+				<a href="blank.html">
+					<i class="menu-icon fa fa-caret-right"></i>
+					Blank Page
+				</a>
+
+				<b class="arrow"></b>
+			</li>
+		</ul>
+	</li>
+</ul><!-- /.nav-list -->
